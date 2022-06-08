bbab6ce5ba356af877104c9a7c8df34532c82ba2
hugo fernandes <oxy.fluid@gmail.com>
2022-06-08 19:59:38 +0100

    Update with playwright custom config and description

--- playwright-python-ui/tests/conftest.py
+++ playwright-python-ui/tests/conftest.py
@@ -1,16 +1,31 @@
+import logging
 import os
 import pytest
 from datetime import datetime
 
-from tests.constants import Constants
+from tests.configuration import Configuration
 
 
+# All the browser context args need to be applied in a single method with the name "browser_context_args" as below
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    if Configuration.add_browser_context_extra_args:
+        args = {
+            **browser_context_args,
+            "viewport": Configuration.view_port,
+            "ignore_https_errors": Configuration.ignore_https_errors
+        }
+        a = logging.getLogger("a")
+        a.error(args)
+        return args
+
+# report filename renaming
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     # to remove environment section
     config._metadata = None
 
-    if not os.path.exists(Constants.reports_dir):
-        os.makedirs(Constants.reports_dir)
+    if not os.path.exists(Configuration.reports_dir):
+        os.makedirs(Configuration.reports_dir)
 
-    config.option.htmlpath = f'{Constants.reports_dir}report_{datetime.now().strftime("%Y%m%d_%H%M%S")}.html'
+    config.option.htmlpath = f'{Configuration.reports_dir}report_{datetime.now().strftime("%Y%m%d_%H%M%S")}.html'
--- playwright-python-ui/tests/constants.py
+++
@@ -1,2 +0,0 @@
-class Constants:
-    reports_dir = "target/reports/"
