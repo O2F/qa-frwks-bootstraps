3217f6afb35c18e0204409535ce2e247212fc6c3
hugo fernandes <oxy.fluid@gmail.com>
2022-06-09 18:50:06 +0100

    Update test code with more assertions

--- playwright-python-ui/tests/test_demo.py
+++ playwright-python-ui/tests/test_demo.py
@@ -18,33 +18,42 @@ def catalog_page(page: Page):
     yield catalog_page
 
 
-def test_one(login_page, catalog_page):
+def test_valid_user_login(login_page, catalog_page):
     valid_user = User("standard_user", "secret_sauce")
     login_page.goto_login_page()
-    login_page.login_page_loaded()
-    login_page.login(valid_user)
+    login_page.is_loaded()
+    login_page.do_login_with(valid_user)
     catalog_page.is_loaded()
     catalog_page.validate_title_is("Products")
     catalog_page.validate_item_nr_text_is(0, "Sauce Labs Backpack")
     catalog_page.iter_items_names()
 
 
-def test_two(login_page):
+def test_invalid_user_login(login_page):
+    user = User("invalid_user", "secret_sauce")
+    login_page.goto_login_page()
+    login_page.is_loaded()
+    login_page.do_login_with(user)
+    login_page.validate_error_message_equals("Epic sadface: Username and password do not match any user in this service")
+
+
+def test_locked_user_login(login_page):
     user = User("locked_out_user", "secret_sauce")
     login_page.goto_login_page()
-    login_page.login_page_loaded()
-    login_page.login(user)
+    login_page.is_loaded()
+    login_page.do_login_with(user)
+    login_page.validate_error_message_equals("Epic sadface: Sorry, this user has been locked out.")
 
 
-def test_three(login_page):
-    user = User("problem_user", "secret_sauce")
-    login_page.goto_login_page()
-    login_page.login_page_loaded()
-    login_page.login(user)
-
-
-def test_four(login_page):
-    user = User("performance_glitch_user", "secret_sauce")
-    login_page.goto_login_page()
-    login_page.login_page_loaded()
-    login_page.login(user)
+# def test_three(login_page):
+#     user = User("problem_user", "secret_sauce")
+#     login_page.goto_login_page()
+#     login_page.is_loaded()
+#     login_page.do_login_with(user)
+#
+#
+# def test_four(login_page):
+#     user = User("performance_glitch_user", "secret_sauce")
+#     login_page.goto_login_page()
+#     login_page.is_loaded()
+#     login_page.do_login_with(user)
