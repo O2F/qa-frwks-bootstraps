bb66718bc35b07a2ca300e00af4cb27151d2205f
hugo fernandes <oxy.fluid@gmail.com>
2022-06-09 18:50:06 +0100

    Update test code with more assertions

--- playwright-python-ui/pages/sauce_demo_pages/login_page.py
+++ playwright-python-ui/pages/sauce_demo_pages/login_page.py
@@ -5,26 +5,32 @@ from model.user import User
 class LoginPage:
 
     _logo=".login_logo"
-
     _username_selector = "#user-name"
     _password_selector = "#password"
     _login_button_selector = "#login-button"
+    _mascot_image = ".bot_column"
+    _error_message_box = "h3[data-test='error']"
 
     def __init__(self, page: Page):
         self.page = page
 
-    def login_page_loaded(self):
+    def is_loaded(self) -> None:
         expect(self.page.locator(self._logo)).to_be_visible()
         expect(self.page.locator(self._username_selector)).to_be_visible()
         expect(self.page.locator(self._password_selector)).to_be_visible()
         expect(self.page.locator(self._login_button_selector)).to_be_visible()
+        expect(self.page.locator(self._mascot_image)).to_be_visible()
 
     def goto_login_page(self) -> None:
-        self.page.goto("https://www.saucedemo.com/")
+        self.page.goto("/")
 
-    def login(self, user: User) -> None:
+    def do_login_with(self, user: User) -> None:
         self.page.locator(self._username_selector).click()
         self.page.locator(self._username_selector).type(user.username)
         self.page.locator(self._password_selector).click()
         self.page.locator(self._password_selector).type(user.password)
         self.page.locator(self._login_button_selector).click()
+
+    def validate_error_message_equals(self, message: str) -> None:
+
+        expect(self.page.locator(self._error_message_box)).to_have_text(message)
