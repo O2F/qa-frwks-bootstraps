78685451e00253ebea3d048a0adaa16f89fa22c6
hugo fernandes <oxy.fluid@gmail.com>
2022-06-11 18:53:19 +0100

    Update workflow to load from secrets

--- playwright-python-ui/tests/configuration.py
+++ playwright-python-ui/tests/configuration.py
@@ -1,13 +1,14 @@
 class Configuration:
-    reports_dir = "target/reports/"
-    # example -> view_port = [800, 600] or view_port = [] for default
-
-    add_browser_context_extra_args = True
-
-    # browser_context_extra_args
-    view_port = {
-                "width": 800,
-                "height": 600
-            }
-
-    ignore_https_errors = False
+    pass
+    # reports_dir = "target/reports/"
+    # # example -> view_port = [800, 600] or view_port = [] for default
+    #
+    # add_browser_context_extra_args = True
+    #
+    # # browser_context_extra_args
+    # view_port = {
+    #             "width": 800,
+    #             "height": 600
+    #         }
+    #
+    # ignore_https_errors = False
