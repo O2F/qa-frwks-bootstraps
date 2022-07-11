d7cb1e5b951d7c093a25985a5a1809844061cfe9
hugo fernandes <oxy.fluid@gmail.com>
2022-07-11 16:02:45 +0100

    Update func to create random string

--- python-requests-graphql-api/utils/string_utils.py
+++ python-requests-graphql-api/utils/string_utils.py
@@ -2,6 +2,22 @@ import string
 import random
 
 
-def create_random_string(string_len: int = 10):
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(string_len))
+def create_random_string(string_len: int = 1,
+                         with_letters: bool = True,
+                         with_numbers: bool = False,
+                         with_symbols: bool = False) -> str:
+    """
+    Creates a random string based on the options provided
+    """
+    letters = ''
+    nr = ''
+    symb = ''
+
+    if with_letters:
+        letters = string.ascii_letters
+    if with_numbers:
+        nr = string.digits
+    if with_symbols:
+        symb = string.punctuation
+
+    return ''.join(random.choice(letters + nr + symb) for i in range(string_len))
